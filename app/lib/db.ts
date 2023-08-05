23ea8c1e90c608923089a531a87b355f0c16d9f5
Isztof <mariuszseget77@gmail.com>
2023-08-05 20:29:02 +0200

    creating the set up for google authentication

--- app/lib/db.ts
+++ app/lib/db.ts
@@ -4,7 +4,7 @@ const url = process.env.UPSTASH_REDIS_REST_URL;
 const token = process.env.UPSTASH_REDIS_REST_TOKEN;
 
 if (!url || !token) {
-  throw new Error('UPSTASH_REDIS_REST_URL or UPSTASH_REDIS_REST_TOKEN is missing in environment variables.');
+  throw new Error('UPSTASH_REDIS_REST_URL or UPSTASH_REDIS_REST_TOKEN is missing in environment variables. Das liegt daran dass die .env.local Datei, in der die env variablen gespeichert werden, von meinem PC nicht gepusch wurde ');
 }
 
 export const db = new Redis({
