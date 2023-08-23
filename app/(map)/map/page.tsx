b8e4faa9c18c17c714ef8eb2132d14e5930c3e0a
Isztof <mariuszseget77@gmail.com>
2023-08-23 16:05:34 +0200

    Some adjustments for the midterm pitch

--- app/(map)/map/page.tsx
+++ app/(map)/map/page.tsx
@@ -2,9 +2,13 @@ import Map from "../map/Map"
 
 function MyPage() {
   return (
-    <div>
-      <h1>My Page</h1>
+    <div style={{backgroundColor: "whitesmoke"}}>
+      <div  style={{width: "400px",
+       height: "300px"       
+    }}className="h-300"> </div> 
+    <div className="max-w-6xl mx-auto"> 
       <Map address="Dortmund, Germany" /> 
+      </div>
     </div>
   );
 }
