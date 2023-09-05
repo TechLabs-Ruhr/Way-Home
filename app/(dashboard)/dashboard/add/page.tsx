37821202e44bf37b3196eaddcf99605b3beee52a
Isztof <mariuszseget77@gmail.com>
2023-09-05 15:18:37 +0200

    Form validation part 1

--- app/(dashboard)/dashboard/add/page.tsx
+++ app/(dashboard)/dashboard/add/page.tsx
@@ -1,8 +1,14 @@
+import AddFriendButton from "@/components/ui/AddFriendButton"
 import {FC} from "react"
 
-const Page: FC<pageProps> = ({}) => {
+
+
+const Page: FC = ({}) => {
     return (
-      <> </>
+    <>
+      <h1 className="font-bold text-5xl mb-8">Welcome to our network!</h1>
+      <AddFriendButton/>
+    </>
     )
   }
   
---
+++ lib/validations/add-friend.ts
@@ -0,0 +1,10 @@
+import { z } from 'zod'
+
+
+export const addFriendValidator = z.object({
+    email: z.string().email(),
+})
+
+const random = {
+    idk: 'asd',
+}
